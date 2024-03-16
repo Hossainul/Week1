d1db3ea3e773fc11108d31bccbb1d11af1333dad
Hossainul <131842237+Hossainul@users.noreply.github.com>
2024-03-16 20:01:57 +0600

    Add files via upload

---
+++ Day 1/B_Number_of_Smaller.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,m;
+    cin >> n >> m;
+
+    vector<int> a(n),b(m);
+
+    for(int i = 0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    for(int i = 0; i<m; i++)
+    {
+        cin >> b[i];
+    }
+
+    int l = 0;
+    int r = 0;
+    
+    int ans = 0;
+    while (r < m)
+    {
+        //cout << l << "->" << r << endl;
+
+        while (b[r] > a[l] && l < n)
+        {
+            /* code */
+            ans ++;
+            l++;
+        }
+        cout << ans << " ";
+        r++;
+    }
+    
+    return 0;
+}
